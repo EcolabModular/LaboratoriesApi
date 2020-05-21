009f4d4cf6ba2d81eb25c4052d2e5ae13ebfcd0e
javicuc-ryrx <javier.lopez@alumnos.udg.mx>
2020-05-21 03:48:59 -0500

    Estructura Final LaboratoriesApi

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -59,6 +59,10 @@ $app->singleton(
 |
 */
 
+$app->middleware([
+    App\Http\Middleware\AuthenticateAccess::class
+]);
+
 // $app->middleware([
 //     App\Http\Middleware\ExampleMiddleware::class
 // ]);
